20b7c9b3aa4ab04da4f27a7612f47c0188656adc
seifElislam <seif.eleslam1990@gmail.com>
2020-04-04 01:02:22 +0200

    Add book shelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BookSelf = (props) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                <li></li>
+            </ol>
+        </div>
+    </div>
+);
+
+export default BookSelf
